37a323d9ce6341e308927d349ca6802cf4d6cdc3
Administrator <Administrator@PC-20170825HQQZ>
2017-09-05 13:31:50 +0800

    拦截器

---
+++ src/ssm/interceptors/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package ssm.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)throws Exception {
+		String url = request.getRequestURI();
+		//判断是否为公开地址
+		if(url.indexOf("login.action")>=0){
+			return true;
+		}
+		String username = (String)request.getSession().getAttribute("username");
+		if(username != null){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
